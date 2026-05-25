93f1c223492d9369939d4e4ef48c614ec4907792
Fabiana V. Velasco <fabiana.vargasv@urosario.edu.co>
2026-05-25 13:32:04 -0500

    Enhance comment for QApplication event loop
    Added comment to clarify the purpose of app.exec()

--- proyecto_urls_fabiana/main.cpp
+++ proyecto_urls_fabiana/main.cpp
@@ -1,9 +1,10 @@
 #include <QApplication>
 #include "mainwindow.h"
 
+// Se crear el objeto QApplication, el cual, maneja el ciclo de eventos, todo lo visual, los argumentos del sistema operativo y mostrar la ventana principal
 int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     MainWindow w;
     w.show();
-    return app.exec();
+    return app.exec();        //Pone el programa en un bucle infinito que espera eventos que haga el usuario (clicks) y los despacha a donde corresponda
 }
